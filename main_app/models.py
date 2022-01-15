62a19a37c975b4840c7e9dec97e0f2ce425a2f04
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2022-01-14 16:18:30 -0800

    Complete Tshirt model & index page

--- main_app/models.py
+++ main_app/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-
+class Tshirt(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Tshirt
 
 # Create your views here.
 def home(request):
@@ -24,3 +25,8 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+def tshirts_index(request):
+    tshirts = Tshirt.objects.all()
+    return render(request, 'tshirts/index.html', {'tshirts': tshirts})
+
